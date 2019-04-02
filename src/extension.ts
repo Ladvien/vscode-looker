a6853b87d5658a81be38ecb90cc6c27d95f5111c
ladvien <honeysucklehardware@gmail.com>
2019-04-02 05:22:39 -0500

    Added allowJs: true

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,21 @@
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Keytar } from './utils/keytar';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+	let keytar = Keytar.tryCreate();
+	console.log(keytar);
+
 	console.log('Congratulations, your extension "looker" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('looker.sayHello', () => {
-		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('sdf World!');
+	let disposable = vscode.commands.registerCommand('looker.sayHello', () => {
+		vscode.window.showInformationMessage('asdasd World!');
 	});
 
 	context.subscriptions.push(disposable);
